69fda4a50c432e0c00a0862f86b9cdef6dc77266
mdhossan <mrhossan2006@gmail.com>
2017-07-11 21:42:34 -0400

    chase colon

--- RifatTest/src/test/java/RifatTest/RifatTest/chase.java
+++ RifatTest/src/test/java/RifatTest/RifatTest/chase.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -26,6 +28,13 @@ public class chase {
 	 driver.navigate().refresh();
 	 String AdvPresnt1 = driver.findElement(By.xpath(".//*[contains(@id,'DA_')]")).getText();
 	 System.out.println(AdvPresnt1);
+	 String AdvPresnt2 = driver.findElement(By.xpath(".//*[contains(@id,'DA_')]")).getText();
+	// System.out.println(AdvPresnt2);
+	// WebDriverWait wait = new WebDriverWait(driver,20);
+	// wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(".//*[@id='DA_649260303933']/div[2]"))).click();
+	 driver.findElement(By.xpath(".//*[@id='userId-input-field'][@placeholder='Username']")).sendKeys("debazroy2");
+	 driver.findElement(By.id("password-input-field")).sendKeys("Nabonita2");
+	 driver.findElement(By.xpath(".//*[@id='signin-module']/div/div/div/div/form/div/div/a[contains(text(),'Sign in')]")).click();
   }
   
   
